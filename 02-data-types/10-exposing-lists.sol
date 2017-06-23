0bd8bbd0762ab3fff5843c6659b88f12e95fd7eb
Roderik van der Veer <roderik@settlemint.io>
2017-06-23 18:29:03 +0200

    Small typos

--- 02-data-types/10-exposing-lists.sol
+++ 02-data-types/10-exposing-lists.sol
@@ -3,18 +3,18 @@ pragma solidity ^0.4.11;
 contract Example {
 
   mapping (address => uint) public ages;
-  addresses[] public ageIndex;
+  address[] public ageIndex;
 
   function setAge(uint age){
     ages[msg.sender] = age;
     ageIndex.push(msg.sender);
   }
 
-  function getAgeIndexLength() constant return (uint) {
+  function getAgeIndexLength() constant returns (uint) {
     return ageIndex.length;
   }
 
-  function getAgeAtIndex(uint index) constant return (address user, uint age) {
+  function getAgeAtIndex(uint index) constant returns (address user, uint age) {
     user = ageIndex[index];
     age = ages[user];
   }
